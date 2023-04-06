cd4fb5c2aa7aef917afba7c7bffcc5026643d8c9
unknown <brenolimac17@gmail.com>
2023-04-06 16:06:43 -0300

    Exercício 3 pronto

---
+++ quarto_exercicio.py
@@ -0,0 +1,9 @@
+conta_consoantes = ("número", "áureo", "micto", "mundo", "maior", "nunca", "copa", "trivela", "correu", "canarinho")
+
+consoantes = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+
+
+
+
+
--- terceiro_exercício.py
+++ terceiro_exercício.py
@@ -1,9 +1,11 @@
 
-Nota1 = (2)
-Nota2 = (6)
-Nota3 = (10)
-Nota4 = (5)
-media = Nota1 + Nota2 + Nota3 + Nota4
-media = media / 4
-print("a media do aluno foi:", media)
+notas = [2, 4, 10, 9, 6]
+media = 0.0
+alunos = ['Vitor', 'Bruno', 'Lucas']
+print (notas[0])
+for valorIndex in notas:
+   media= media + valorIndex
+media= media/ len(notas)
+print(media)
+
 
