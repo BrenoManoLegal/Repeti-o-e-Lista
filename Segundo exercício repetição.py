e425eb8981168c99ea5c3e096c60bf0f3cd116c8
unknown <brenolimac17@gmail.com>
2023-04-14 11:13:12 -0300

    Exercício 2 concluido sobre repetição

---
+++ Segundo exercício repetição.py
@@ -0,0 +1,5 @@
+Nome = str (input('Nome de Usuário: '))
+Senha = (input('Senha: '))
+while Nome == Senha or Senha == Nome:
+    Nome = str (input('Nome de Usuário: '))
+    Senha = (input('Senha: '))
